c0f6e25ae81ebc678e9fc978db7979ef1fe1d495
Samuel Blackburn <samuel.blackburn@nbrown.co.uk>
2022-03-29 12:41:38 +0100

    Initial Commit

---
+++ SwiftUI GridLayouts/SwiftUI_GridLayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GridLayoutsApp.swift
+//  SwiftUI GridLayouts
+//
+//  Created by Samuel Blackburn on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GridLayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
